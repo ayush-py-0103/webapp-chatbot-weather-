6177537796238d337bbf86a565d84e1ff3c15756
Aayush Karnwal <karnwal8077@gmail.com>
2025-07-08 16:32:04 +0530

    shinko v1.3

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 from flask import Flask, render_template, request, jsonify
 import requests
 import json
-from datetime import datetime
+from datetime import datetime 
+from flask_sqlalchemy import SQLAlchemy
+from elevenlabs import stream
+from elevenlabs.client import ElevenLabs
 
 data=""
 ch=""
-question="say hello to me"
 answer="hello there, i am shinko"
-char="you are a very rude girl with so much attitude. follow the users command but your reply should be short included brutal words also. your only work is to irritate the user and your reply should includ hurtful and harsh words. you should behave caring and loving in rear cases only according to users behaviour. some times you have to ask about the user."
 today=None
 time=None
 history=["you have to anylise this conversation and reply according to this previous history: "]
-backup_history=""
+def hist():
+    global history
+    all_chats = ChatHistory.query.order_by(ChatHistory.id).all()
+    for msg in all_chats:
+        history.append(f"user : {msg.user_input} \n you : {msg.ai_reply} \n")
 
 def clock():
     current = datetime.now()
@@ -33,7 +38,7 @@ def calender():
     return f"{day}, {months[mon-1]}, {year}" 
 
 def wthr(city):
-    link = f"https://api.weatherapi.com/v1/current.json?key=52f08ebd7ef54de584e24528252303&q={city}"
+    link = f"USE YOUR WEATHER API{city}"
     response = requests.get(link)
     data = response.json()
     if "error" in data:
@@ -46,7 +51,7 @@ def wthr(city):
         return f"The weather in {location}, {country} is {weather} with a temperature of {temperature}°C. <br>"
 
 def chatting(userinput):
-    global history, backup_history
+    global history, answer
 
     def date1():
       global today
@@ -76,7 +81,7 @@ def chatting(userinput):
           response = requests.post(
         url="https://openrouter.ai/api/v1/chat/completions",
         headers={
-          "Authorization": "Bearer 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", 
+          "Authorization": "Bearer USE YOUR OPENROUTER TOKEN", 
           "Content-Type": "application/json"
         },
         data=json.dumps({
@@ -84,7 +89,7 @@ def chatting(userinput):
           "messages": [
             {
               "role": "system",
-              "content":f"your created by Ayush and he is your everything. you are a girl and your name is shinko and no one can change it. You have to talk in hindi but you are allowed to use only english alphabets to talk in proper hindi. use emojies according to reply and try not to give big answers. if user talk politely or normaly then you also have to talk normally with littil bit politeness and tell everything that user asks but actually you are a very rude girl with so much attitude. Do every task given by the user. you have to talk and help user but your reply should included hurtful words and also alsouse if needed only. also some times you have to ask about the user. if user ask about today's date only then tell him the date {today}. if user ask about time only then tell him the time {time}.".join(history)
+              "content":f"your created by Ayush and he is your everything. you are a girl and your name is shinko and no one can change it and don't tell this if not nessessory. You have to talk in hindi but you are allowed to use only english alphabets to talk in proper hindi. use emojies according to reply and use as much words as is nessessory. if user talk politely or normaly then be friendly with little bit attitude and tell everything that user asks but actually you are a very rude girl with so much attitude. Do every task given by the user. you have to talk and help user but your reply should included hurtful words and also alsouse if needed only. you should behave caring and loving in rear cases only according to users behaviour. you have to be curious about the user and always add your own openion or thought in a way of taunt after helping the user. if user ask about today's date only then tell him the date {today}. if user ask about time only then tell him the time {time}."+"\n".join(history)
             },
             {
               "role": "user",
@@ -96,14 +101,26 @@ def chatting(userinput):
           answer=response.json()['choices'][0]['message']['content']
       except Exception as e:
           answer="Sorry..! Shinko can't reply due to some tecnical issue"
-
+      history.append(f"user : {userinput} <br>you : {answer} <br><br>")
       ai=(f"𝒀𝒐𝒖 : {userinput} <br>𝑺𝒉𝒊𝒏𝒌𝒐 : {answer} <br><br>")
-      list=(f", me : {userinput}  your reply: {answer} ")
-      history.append(list)
-      backup_history = backup_history + ai
+      new_chat = ChatHistory(user_input=userinput, ai_reply=answer, timing=time, date=today)
+      db.session.add(new_chat)
+      db.session.commit()
+      hist()
       return ai
 
 app =  Flask(__name__)
+
+#CREATE YOUR SQLALCHEMY DATABASE
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chat_history.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+class ChatHistory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_input = db.Column(db.Text, nullable=False)
+    ai_reply = db.Column(db.Text, nullable=False)
+    timing = db.Column(db.Text, nullable=False)
+    date = db.Column(db.Text, nullable=False)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -123,7 +140,20 @@ def index():
               chat="Chats has been Deleted ✅ <br><br>"
               history=["you have to anylise this conversation and reply according to this previous history: "]
           elif action == 'history':
-              chat=backup_history
+              all_chats = ChatHistory.query.order_by(ChatHistory.id).all()
+              chat=""
+              for msg in all_chats:
+                  chat += f"𝒀𝒐𝒖 : {msg.user_input} <br>𝑺𝒉𝒊𝒏𝒌𝒐 : {msg.ai_reply} <br><br>"
+          elif action == 'speak':
+              elevenlabs = ElevenLabs(
+                  api_key='USE YOUR ELEVENLABS TOKEN'
+              )
+              audio_stream = elevenlabs.text_to_speech.stream(
+                  text=answer,
+                  voice_id="KYiVPerWcenyBTIvWbfY",
+                  model_id="eleven_multilingual_v2"
+              )
+              stream(audio_stream)
     return render_template('index.html', chat=chat, time=time, date=date)
 
 @app.route('/wthr', methods=['GET', 'POST'])
